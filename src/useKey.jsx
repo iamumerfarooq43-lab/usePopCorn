2d85ec4ff73b0f20d510278b6ffb6d113acdfec2
Umer Farooq <iamumerfarooq@gmail.com>
2026-02-27 00:08:51 +0500

    Initial commit

---
+++ src/useKey.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+const useKey = (key, action) => {
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code.toLowerCase() === key.toLowerCase()) {
+          action();
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [key, action],
+  );
+};
+
+export default useKey;
---
+++ src/useMovies.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+
+const KEY = "aaccc867";
+
+const useMovies = (query) => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      // callback?.();
+
+      const controller = new AbortController();
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+
+          const res = await fetch(
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal },
+          );
+
+          if (!res.ok) {
+            console.log("Response status:", res.status);
+            throw new Error("Something went wrong with fetching movies");
+          }
+
+          const data = await res.json();
+          console.log("API Response:", data);
+          if (data.Response === "False") throw new Error("Movie not found");
+
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            console.log("Fetch Error:", err.message);
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      // handleCloseMovie();
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query],
+  );
+  return { movies, isLoading, error };
+};
+
+export default useMovies;
